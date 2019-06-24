06691664e3fc37e48b8028806d9e45becde472e1
barry <barry.allen@kakaobank.com>
2019-06-24 13:44:18 +0900

    There are no duplicates in the list. Find Missing Number algorithm

--- src/main/java/com/jeonguk/algorithm/find/FindMissingNumberMain.java
+++ src/main/java/com/jeonguk/algorithm/find/FindMissingNumberMain.java
@@ -3,10 +3,8 @@ package com.jeonguk.algorithm.find;
 public class FindMissingNumberMain {
 
     public static int getMissingNo(int[] arr, int n) {
-        int i;
-        int total;
-        total = (n + 1) * (n + 2) / 2;
-        for (i = 0; i < n; i++) {
+        int total = (n + 1) * (n + 2) / 2;
+        for (int i = 0; i < n; i++) {
             total -= arr[i];
         }
         return total;
